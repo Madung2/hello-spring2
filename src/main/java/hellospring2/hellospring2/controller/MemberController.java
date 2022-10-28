b5e01b917ae5e6def1d859616b9382696007f502
DESKTOP-HL5MIJ7\tulip <tuliphan91@gmail.com>
2022-10-28 10:09:28 +0900

    MVC

--- src/main/java/hellospring2/hellospring2/controller/MemberController.java
+++ src/main/java/hellospring2/hellospring2/controller/MemberController.java
@@ -1,8 +1,14 @@
 package hellospring2.hellospring2.controller;
 
+import hellospring2.hellospring2.domain.Member;
 import hellospring2.hellospring2.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller //스프링에서 스프링빈이 관리됨, 스프링 컨테이너에 등록하고 하나의 등록된 객체를 사용할거임
 public class MemberController {
@@ -11,5 +17,26 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
---
+++ src/main/java/hellospring2/hellospring2/controller/MemberForm.java
@@ -0,0 +1,14 @@
+package hellospring2.hellospring2.controller;
+
+public class MemberForm {
+    private String name;
+    //템플릿의 네임이 여기에 매치
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
